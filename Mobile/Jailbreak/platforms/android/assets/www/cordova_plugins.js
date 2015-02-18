963dd1cf47eb765042d07eb00b1dcb5327ee4d78
vashX30a <vashX30a@gmail.com>
2015-02-19 05:33:52 +0800

    Revert "compass plugin"
    This reverts commit b8c97b9a655ec1a7ea2438ab03b8c4f940d9dcc3.

--- Mobile/Jailbreak/platforms/android/assets/www/cordova_plugins.js
+++ Mobile/Jailbreak/platforms/android/assets/www/cordova_plugins.js
@@ -6,34 +6,12 @@ module.exports = [
         "clobbers": [
             "navigator.splashscreen"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device-orientation/www/CompassError.js",
-        "id": "org.apache.cordova.device-orientation.CompassError",
-        "clobbers": [
-            "CompassError"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device-orientation/www/CompassHeading.js",
-        "id": "org.apache.cordova.device-orientation.CompassHeading",
-        "clobbers": [
-            "CompassHeading"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device-orientation/www/compass.js",
-        "id": "org.apache.cordova.device-orientation.compass",
-        "clobbers": [
-            "navigator.compass"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.splashscreen": "0.3.5",
-    "org.apache.cordova.device-orientation": "0.3.11"
+    "org.apache.cordova.splashscreen": "0.3.5"
 }
 // BOTTOM OF METADATA
 });
--- Mobile/Jailbreak/platforms/android/assets/www/js/app.js
+++ Mobile/Jailbreak/platforms/android/assets/www/js/app.js
@@ -1,115 +1,35 @@
 (function ($) {
- // rAF
-  window.requestAnimationFrame = function() {
-  return window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    function(f) {
-      window.setTimeout(f,1e3/60);
-    }
-  }();
 
-  var canvas = document.querySelector('canvas');
-  var ctx = canvas.getContext('2d');
-  var stop = false;
-  var W = canvas.width;
-  var H = canvas.height;
 
-// We want to move/slide/scroll the background
-// as the player moves or the game progresses
-
-// Velocity X
-var vy = 0;
-
-var img = new Image();
-img.src = 'img/JBGameEWalls.png';
-
-function renderGame() {
-  if (!stop) {
-     window.requestAnimationFrame(renderGame);
-    
-    ctx.clearRect(0, 0, W, H);
-    
-    ctx.fillStyle = '#333';
-    ctx.fillRect(0, 0, 480, 400);
-    
-    ctx.drawImage(img, 0, vy);
-    ctx.drawImage(img, 0, Math.abs(vy)-img.height);
-    
-    if (Math.abs(vy) > img.height) {
-      vy = 0;
-    }
-  
-    vy += 4;
-  };
-
+/**
+ * Show the main menu after loading all assets
+ */
+function mainMenu() {
+  $('#progress').hide();
+  $('#main').show();
+  $('#menu').addClass('main');
 }
-   
-  
 
-  /**********************************************/
-  /**
-   * Show the main menu after loading all assets
-   */
-  function mainMenu() {
-    $('#progress').hide();
-    $('#main').show();
-    $('#menu').addClass('main');
-  }
+/**
+ * Click handlers for the different menu screens
+ */
+$('.option').click(function() {
+  $('#main').hide();
+  $('#options-container').show('slow');
+  $('#menu').addClass('options stretchRight');
+});
 
-  /**
-   * Click handlers for the different menu screens
-   */
-  $('.play').click(function() {
-    $('#menu').hide();
-    $('#pause').show();
-    $('#canvas').show();
-    renderGame();
+$('.help').click(function() {
+  $('#main').hide();
+  $('#help-container').show('slow');
+  $('#menu').addClass('help stretchRight');
+});
 
-  });
+$('.back').click(function() {
+  $('#options-container,#help-container').hide();
+  $('#main').show();
+  $('#menu').removeClass('options help stretchRight');
+});
 
-  $('#pause').click(function() {
-    var $this = $(this);
-    stop = true;
-
-    if ($this.hasClass('pause-off')) {
-      $('#container').show();
-    } 
-  });
-
-  $('.resume').click(function() {
-    stop = false;
-    renderGame();
-    $('#container').hide();
-  });
-
-  $('.quit').click(function() {
-    $('#container').hide();
-    $('#pause').hide();
-    stop = false;
-    $('#menu').show();
-  });
-
-
-  $('.option').click(function() {
-    $('#main').hide();
-    $('#options-container').show('slow');
-    $('#menu').addClass('options stretchRight');
-  });
-
-  $('.help').click(function() {
-    $('#main').hide();
-    $('#help-container').show('slow');
-    $('#menu').addClass('help stretchRight');
-  });
-
-  $('.back').click(function() {
-    $('#options-container, #help-container').hide();
-    $('#main').show();
-    $('#menu').removeClass('options help stretchRight');
-  });
-
-  mainMenu();
+mainMenu();
 })(jQuery);
