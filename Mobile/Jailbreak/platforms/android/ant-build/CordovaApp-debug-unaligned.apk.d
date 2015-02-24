562a00a0c38af2ab6a9814a3ff6e0c9bcffa51b4
vashX30a <vashX30a@gmail.com>
2015-02-25 02:31:30 +0800

    accelerometer plugin

--- Mobile/Jailbreak/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ Mobile/Jailbreak/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\CordovaApp.ap_ \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes.dex \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes.dex \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\CordovaLib\ant-build\classes.jar \
--- Mobile/Jailbreak/platforms/android/ant-build/classes.dex.d
+++ Mobile/Jailbreak/platforms/android/ant-build/classes.dex.d
@@ -1,10 +1,14 @@
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes.dex : \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\BuildConfig.class \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\CordovaApp.class \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R$attr.class \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R$drawable.class \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R$string.class \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R$xml.class \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R.class \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\org\apache\cordova\splashscreen\SplashScreen.class \
-C:\Users\Karen\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\dexedLibs\classes-4e1c967067b3ebf6ccffc57b9849e818.jar \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes.dex : \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\BuildConfig.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\CordovaApp.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R$attr.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R$drawable.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R$string.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R$xml.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\com\CabiliMoso\jailbreaker\R.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener$1.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\org\apache\cordova\deviceorientation\CompassListener$1.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\org\apache\cordova\deviceorientation\CompassListener.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\classes\org\apache\cordova\splashscreen\SplashScreen.class \
+C:\Users\Sherwin-Admin\Documents\GitHub\Jailbreak\Mobile\Jailbreak\platforms\android\ant-build\dexedLibs\classes-7988bba9c3b8bda788765b2d0ec9fa53.jar \
